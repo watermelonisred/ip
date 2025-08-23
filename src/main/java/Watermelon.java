2ecdadfb2f0d7b82717046d616d0bc8a0f9e7b8d
yu xin <lyuxin1108@gmail.com>
2025-08-23 19:54:09 +0800

    Add echo function and exit when bye

--- src/main/java/Watermelon.java
+++ src/main/java/Watermelon.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Watermelon {
     public static void main(String[] args) {
         /* String logo = " ____        _        \n"
@@ -13,15 +15,28 @@ public class Watermelon {
                 + "   \\_/\\_/ \\__,_|\\__\\___|_|  |_| |_| |_|\\___|_|\\___/|_| |_|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("____________________________________________________________");
+        String indent = " ".repeat(4);
 
-        System.out.println(" Hello! I'm Watermelon\n" +
-                " What can I do for you?");
+        System.out.println(indent + "____________________________________________________________");
 
-        System.out.println("____________________________________________________________");
+        System.out.println(indent + " Hello! I'm Watermelon\n" +
+                indent + " What can I do for you?");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(indent + "____________________________________________________________");
 
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        // System.out.println(input);
+        while (!input.equals("bye")) {
+            System.out.println(indent + "____________________________________________________________");
+            System.out.println(indent + input);
+            System.out.println(indent + "____________________________________________________________");
+            input = scanner.nextLine();
+        }
+        scanner.close();
+        System.out.println(indent + "____________________________________________________________");
+        System.out.println(indent + "Bye. Hope to see you again soon!");
+        System.out.println(indent + "____________________________________________________________");
     }
 }
