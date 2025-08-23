e88d5a7e49ebef3e6de0f500b33699659dd0cab6
yu xin <lyuxin1108@gmail.com>
2025-08-23 20:41:31 +0800

    Add list function and added label

--- src/main/java/Watermelon.java
+++ src/main/java/Watermelon.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Watermelon {
     public static void main(String[] args) {
+        String indent = " ".repeat(4);
         /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -15,8 +17,6 @@ public class Watermelon {
                 + "   \\_/\\_/ \\__,_|\\__\\___|_|  |_| |_| |_|\\___|_|\\___/|_| |_|\n";
         System.out.println("Hello from\n" + logo);
 
-        String indent = " ".repeat(4);
-
         System.out.println(indent + "____________________________________________________________");
 
         System.out.println(indent + " Hello! I'm Watermelon\n" +
@@ -27,13 +27,27 @@ public class Watermelon {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        // System.out.println(input);
+        ArrayList<String> tasklist = new ArrayList<>();
         while (!input.equals("bye")) {
             System.out.println(indent + "____________________________________________________________");
-            System.out.println(indent + input);
+            if (input.equals("list")) {
+                // tasklist is empty
+                if (tasklist.size() == 0) {
+                    System.out.println(indent + "tasklist is empty");
+                }
+                // tasklist is not empty
+                for (int i = 0; i < tasklist.size(); i++) {
+                    int task_index = i + 1;
+                    System.out.println(indent + task_index + ". " + tasklist.get(i));
+                }
+            } else {
+                tasklist.add(input);
+                System.out.println(indent + "added: " + input);
+            }
             System.out.println(indent + "____________________________________________________________");
             input = scanner.nextLine();
         }
+        // "bye"
         scanner.close();
         System.out.println(indent + "____________________________________________________________");
         System.out.println(indent + "Bye. Hope to see you again soon!");
